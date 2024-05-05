8e10159aa3333cbad9566bcd76ddfde75e89f1ef
Caleb Barger <bargercj20@hotmail.com>
2024-05-04 17:44:42 -0700

    Slice type

--- base_types.h
+++ base_types.h
@@ -2,7 +2,7 @@
 // base_types.h
 //
 // Caleb Barger
-// 02/09/24 
+// 02/09/24
 //
 
 #ifndef BASE_TYPES_H
@@ -12,8 +12,8 @@
 
 #include <stdint.h>
 #include "stdlib.h"
-#include <string.h> 
-#include <stdio.h> 
+#include <string.h>
+#include <stdio.h>
 
 ////////////////////////////////
 //~ cabarger: Codebase keywords
@@ -43,7 +43,7 @@ typedef double F64;
 
 #define AlignPow2(a, b) ((a + b - 1) & ~(b - 1))
 
-#define AssertBreak(m) (*((volatile U32*)0) = 0xCA1EB)  
+#define AssertBreak(m) (*((volatile U32*)0) = 0xCA1EB)
 #define AssertMessage(m) AssertBreak(m)
 #define InvalidPath AssertMessage("Invalid path")
 #define InvalidCase default: InvalidPath
@@ -55,7 +55,14 @@ typedef double F64;
 #define Max(a, b) (a > b ? a : b)
 #define Min(a, b) (a < b ? a : b)
 
-#define ArrayCount(a) (sizeof(a) / sizeof(a[0]))
+#define ArrayCount(a) (sizeof(a) / sizeof(a[0]))E
+
+typedef struct Slice Slice;
+struct Slice
+{
+    U8* ptr;
+    U64 len;
+};
 
 #define BASE_TYPES_H
 #endif
